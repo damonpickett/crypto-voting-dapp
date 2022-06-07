fd3cb93c450cab75c4c89b21d8123b5d17c942f7
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-06-07 13:08:45 -0700

    made corrections to InstructionModal

--- src/components/InstructionModal.js
+++ src/components/InstructionModal.js
@@ -14,9 +14,9 @@ const InstructionModal = (props) => {
         <div className="modal-body">
           <ul>
               <ol>Connect your Ethereum wallet via the 'Connect Wallet' button in the top right corner</ol>
-              <ol>Connenct your wallet to Polygon's Mumbai testnet</ol>
+              <ol>Connect your wallet to Polygon's Mumbai testnet</ol>
               <ol>You'll need Matic tokens to interact with the blockchain. You can collect mock Matic tokens from the <a href='https://faucet.polygon.technology/' target='_blank' rel='noreferrer'>Polygon Faucet</a></ol>
-              <ol>Select 'Up' or 'Down' to vote as to whether you believe the value of the given crypto currency will rise or fall</ol>
+              <ol>Select 'Up' or 'Down' to vote as to whether you believe the value of the given currency will rise or fall</ol>
               <ol>Click the 'INFO' button to learn more about a given currency</ol>
           </ul>
         </div>
@@ -29,7 +29,7 @@ const InstructionModal = (props) => {
               <ol>This application's smart contract has been deployed to <a href='https://mumbai.polygonscan.com/' target='_blank' rel='noreferrer'>Polygon's Mumbai Testnet</a></ol>
               <ol>The contract and its transactions can be viewed <a href='https://mumbai.polygonscan.com/address/0x1570Bbfca7492c2294410b6966609e9b8B2952d8' target='_blank' rel='noreferrer'>here</a></ol>
               <ol>For a breakdown of how this app works, and to view the code, check out the <a href='https://github.com/damonpickett/crypto-voting-dapp' target='_blank' rel='noreferrer'>Github repo</a></ol>
-              <ol>This application was built based on a free <a href='https://www.youtube.com/watch?v=MI_Se26Sfmo' target='_blank' rel='noreferrer'>tutorial</a> provided by <a href='https://moralis.io/' target='_blank' rel='noreferrer'>Moralis</a></ol>
+              <ol>This application was built by following a free <a href='https://www.youtube.com/watch?v=MI_Se26Sfmo' target='_blank' rel='noreferrer'>tutorial</a> provided by <a href='https://moralis.io/' target='_blank' rel='noreferrer'>Moralis</a></ol>
           </ul>
         </div>
         <div className="modal-footer">
